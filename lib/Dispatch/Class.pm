83c5d77a9ee1885b510b6fe599e3a988d27687c8
Lukas Mai <l.mai@web.de>
2014-11-29 15:20:23 +0100

    version 0.02

--- lib/Dispatch/Class.pm
+++ lib/Dispatch/Class.pm
@@ -3,7 +3,7 @@ package Dispatch::Class;
 use warnings;
 use strict;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Scalar::Util qw(blessed);
 
@@ -178,7 +178,7 @@ Lukas Mai, C<< <l.mai at web.de> >>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2013 Lukas Mai.
+Copyright 2013, 2014 Lukas Mai.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
